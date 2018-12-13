b77026631c5984065d2bc2815517c2f614539d9e
wangjiabao520 <wangjiabao520@users.noreply.github.com>
2018-12-13 13:22:02 +0800

    121

---
+++ Hi/src/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package activity;
+
+import com.example.hi.*;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainactivity);
+	}
+	
+}
